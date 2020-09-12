1b0701bdf945f229f94438cc3ec1c006902340fd
Deepak Verma <dpkvrm999git config --global user.email dpkvrm999@gmail.comgit config --global user.email dpkvrm999>
2020-09-12 10:28:50 +0530

    init commit

---
+++ precache-manifest.d4bc552acea307c73cf632c965306d5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0c338a1427c02d8263de2812ae8b61",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9b4dbce89f94c87ec9c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a6ed62f536fadda01aee",
+    "url": "/static/js/2.7593fd26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7593fd26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9b4dbce89f94c87ec9c",
+    "url": "/static/js/main.504741d0.chunk.js"
+  },
+  {
+    "revision": "4025d8f70fa92f61d356",
+    "url": "/static/js/runtime-main.fb91708a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
